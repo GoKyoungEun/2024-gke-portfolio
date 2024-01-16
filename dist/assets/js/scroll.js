dbf9c80f0185fc11824d771ea5510d3c50ff6732
kyoungeun <seven2649@naver.com>
2024-01-16 23:01:18 +0900

    포트폴리오 작업

---
+++ dist/assets/js/scroll.js
@@ -0,0 +1,149 @@
+let animationCompleted = false;
+function animateSVGPath() {
+  return new Promise(resolve => {
+    if (animationCompleted) {
+      resolve();
+      return;
+    }
+    const svgBox = document.querySelector('.svg-box');
+    const svg = svgBox.querySelector('svg');
+    const path = svgBox.querySelector('svg path');
+    if (!path) {
+      console.error("SVG path not found");
+      resolve();
+      return;
+    }
+    const length = path.getTotalLength();
+
+    // SVG의 opacity를 1로 설정
+    gsap.set(svg, {
+      opacity: 1
+    });
+    gsap.set(path, {
+      strokeDasharray: length,
+      strokeDashoffset: length
+    });
+    gsap.to(path, {
+      strokeDashoffset: 0,
+      duration: 2,
+      ease: "power2.out",
+      onComplete: () => {
+        animationCompleted = true;
+        resolve();
+      }
+    });
+  });
+}
+function resetAnimation() {
+  animationCompleted = false;
+  // .svg-box의 opacity를 0으로 설정
+  gsap.to('.svg-box svg', {
+    opacity: 0,
+    duration: 0.5
+  });
+}
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.create({
+  trigger: ".svg-box",
+  start: "top center",
+  // .svg-box가 뷰포트 아래쪽에 닿을 때
+  end: "bottom top",
+  // .svg-box가 뷰포트 위쪽으로 사라질 때
+  onEnter: animateSVGPath,
+  onLeave: resetAnimation,
+  onEnterBack: animateSVGPath,
+  onLeaveBack: resetAnimation
+});
+function getWorkListWidth() {
+  const workList = document.querySelector('.work-list');
+  return workList ? workList.offsetWidth : 0; // workList가 존재하면 너비 반환, 그렇지 않으면 0 반환
+}
+
+ScrollTrigger.create({
+  trigger: ".sec-portfolio-work",
+  start: "top top",
+  // .page2의 상단이 뷰포트 상단에 도달했을 때 시작
+  end: () => `+=${getWorkListWidth()}`,
+  // 6000px 만큼 스크롤할 동안 애니메이션 진행
+  pin: true,
+  // .page2를 고정
+  scrub: true // 스크롤 위치에 따라 애니메이션 조정
+});
+
+gsap.to(".content-wrap", {
+  x: () => -getWorkListWidth(),
+  // .content-wrap의 이동 거리를 동적으로 계산
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".sec-portfolio-work",
+    start: "top top",
+    end: () => `+=${getWorkListWidth()}`,
+    // .work-list의 너비만큼 스크롤 구간 설정
+    scrub: true
+  }
+});
+document, addEventListener('DOMContentLoaded', () => {
+  new TypeIt("#mainTitle", {
+    speed: 150,
+    startDelay: 1400
+  }).type("KYOUNG eun", {
+    delay: 100
+  }).delete(3).type("EUN", {
+    delay: 100
+  }).type("<br>PORTFOLIO", {
+    delay: 100
+  }).go();
+});
+const pTag1 = document.querySelector('.first-parallel');
+const pTag2 = document.querySelector('.second-parallel');
+const textArr1 = 'HTML CSS JavaScript SCSS jQuery Animation React Node Git Figma VSCode HTML CSS JavaScript SCSS jQuery Animation React Node Git Figma VSCode'.split(' ');
+const textArr2 = 'HTML CSS JavaScript SCSS jQuery Animation React Node Git Figma VSCode HTML CSS JavaScript SCSS jQuery Animation React Node Git Figma VSCode'.split(' ');
+let count1 = 0;
+let count2 = 0;
+initTexts(pTag1, textArr1);
+initTexts(pTag2, textArr2);
+function initTexts(element, textArray) {
+  textArray.push(...textArray);
+  for (let i = 0; i < textArray.length; i++) {
+    element.innerText += `${textArray[i]}\u00A0\u00A0\u00A0\u00A0`;
+  }
+}
+function marqueeText(count, element, direction) {
+  if (count > element.scrollWidth / 2) {
+    element.style.transform = `translate3d(0, 0, 0)`;
+    count = 0;
+  }
+  element.style.transform = `translate3d(${direction * count}px, 0, 0)`;
+  return count;
+}
+function animate() {
+  count1++;
+  count2++;
+  count1 = marqueeText(count1, pTag1, -1);
+  count2 = marqueeText(count2, pTag2, 1);
+  window.requestAnimationFrame(animate);
+}
+function scrollHandler() {
+  count1 += 15;
+  count2 += 15;
+}
+window.addEventListener('scroll', scrollHandler);
+animate();
+$('.portfolio-about-text p strong').mouseenter(function () {
+  let changeImage = $(this).attr('data-img');
+  let changeRotateImage = $(this).attr('data-rotate-img');
+  $('.portfolio-about-img .nomal-img').css({
+    'background-image': 'url(' + changeImage + ')'
+  });
+  $('.portfolio-about-img .rotate-img').css({
+    'background-image': 'url(' + changeRotateImage + ')'
+  });
+});
+$('.portfolio-about-text p strong').mouseleave(function () {
+  $('.portfolio-about-img .nomal-img').css({
+    'background-image': ''
+  });
+  $('.portfolio-about-img .rotate-img').css({
+    'background-image': ''
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ const sourcemaps = require("gulp-sourcemaps");
 // js
 const babel = require("gulp-babel");
 const minify = require("gulp-minify");
+//서버
+const connect = require('gulp-connect');
 
 // 상수에 대문자를 사용하는 것이 일반적인 규칙, 상수는 한 번 정의하면 수정할 수 없는 값으로, 대문자로 정의하면 수정 가능한 일반 변수와 구분
 // PATH코드의 경우 다양한 자산에 대한 경로를 포함하는 상수 개체입니다. 대문자를 사용하면 프로그램 실행 중에 이러한 경로를 수정해서는 안 된다는 점을 다른 개발자에게 분명히 알릴 수 있습니다.
@@ -44,16 +46,18 @@ gulp.task("html", () => {
         basepath: "@file",
       })
     )
-    .pipe(gulp.dest("./dist/"));
+    .pipe(gulp.dest("./dist/"))
+    .pipe(connect.reload());
 });
 
 gulp.task("scss", () => {
   return gulp
-    .src(PATH.ASSETS.STYLE + "/**/*.scss")
+    .src(PATH.ASSETS.STYLE + "/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: "compressed", sourceMap: true }).on("error", sass.logError))
     .pipe(sourcemaps.write(".", { sourceRoot: "../../src" }))
-    .pipe(gulp.dest("./dist/assets/css"));
+    .pipe(gulp.dest("./dist/assets/css"))
+    .pipe(connect.reload());
 });
 
 gulp.task("js", () => {
@@ -61,7 +65,8 @@ gulp.task("js", () => {
     .src(PATH.ASSETS.JS + "/**/*.js")
     .pipe(babel())
     .pipe(minify())
-    .pipe(gulp.dest("./dist/assets/js"));
+    .pipe(gulp.dest("./dist/assets/js"))
+    .pipe(connect.reload());
 });
 
 gulp.task("images", () => {
@@ -69,15 +74,27 @@ gulp.task("images", () => {
     return gulp
       .src(PATH.ASSETS.IMG + "/**/*")
       .pipe(imagemin.default())
-      .pipe(gulp.dest("./dist/assets/images"));
+      .pipe(gulp.dest("./dist/assets/images"))
+      .pipe(connect.reload());
   });
 });
 
 gulp.task("build", gulp.parallel("scss", "js", "html", "images"));
 
-// watch
+
 gulp.task("watch", () => {
   gulp.watch(PATH.ASSETS.STYLE + "/**/*.scss", gulp.series("scss"));
   gulp.watch(PATH.ASSETS.JS + "/**/*.js", gulp.series("js"));
   gulp.watch(PATH.ASSETS.HTML + "/*.html", gulp.series("html"));
 });
+
+gulp.task('webserver', function(done) {
+  connect.server({
+    root: 'dist', // 서버의 루트 디렉토리
+    livereload: true, // 라이브 리로드 활성화
+    port: 8080 // 포트 설정
+  });
+  done();;
+});
+
+gulp.task('dev', gulp.series('webserver', 'watch'));
